c43034dabb97c454de4e003b8020673b4ed5fcb6
Tahmeem <tahmeemmahedi@gmail.com>
2021-06-22 22:06:38 -0400

    navbar added and code cleanup

--- Art/views.py
+++ Art/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'creator':'Tahmeem',
+        'title': 'Mario jumping',
+        'image': 'something.jpeg'
+    },
+    {
+        'creator':'Bob',
+        'title': 'Luigi jumping',
+        'image': 'something2.jpeg'
+    }
+]
+
+
 def home(request):
-    return render(request, 'Art/home.html')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'Art/home.html',context)
 
 def Profile(request):
     return render(request, 'Art/profile.html')
