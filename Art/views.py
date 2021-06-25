59b00fbd5c168fa17545db774a7fa98ad1f62199
Tahmeem <tahmeemmahedi@gmail.com>
2021-06-25 18:28:52 -0400

    First card with artwork made

--- Art/views.py
+++ Art/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render
 
-posts = [
+Drawings = [
     {
         'creator':'Tahmeem',
-        'title': 'Mario jumping',
-        'image': 'something.jpeg'
+        'title': 'First Drawing',
+        'Price': 50,
+        'description': 'First art to be hosted on the site. A random image from google image.',
+        'image': '/Art/Assets/ArtShowcase/Art1.jpg',
     },
     {
-        'creator':'Bob',
-        'title': 'Luigi jumping',
-        'image': 'something2.jpeg'
+        'creator':'Bob Ross',
+        'title': 'Link',
+        'Price': 50,
+        'description': 'The protagonist from the Legend of Zelda game series.',
+        'image': '/Art/Assets/ArtShowcase/Art1.jpg',
     }
 ]
 
-
 def home(request):
     context = {
-        'posts':posts
+        'Drawings': Drawings
     }
-    return render(request, 'Art/home.html',context)
+    return render(request, 'Art/home.html', context)
 
 def Profile(request):
     return render(request, 'Art/profile.html')
