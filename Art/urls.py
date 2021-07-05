ea03c54c850afd4cc156cd61a0878e9e3f8ee2ac
Tahmeem <tahmeemmahedi@gmail.com>
2021-07-05 01:12:28 -0400

    Home link added to header and class based views used

--- Art/urls.py
+++ Art/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from .views import ArtListView
 from . import views
 urlpatterns = [
-    path('', views.home, name='artwork-home'),
+    path('', ArtListView.as_view(), name='artwork-home'),
     path('profile/', views.Profile, name='artwork-profile'),
 ]
--- Art/views.py
+++ Art/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import artWork
 
 def home(request):
@@ -6,6 +7,13 @@ def home(request):
         'Drawings': artWork.objects.all()
     }
     return render(request, 'Art/home.html', context)
+
+class ArtListView(ListView):
+    model = artWork
+    template_name = 'Art/home.html'
+    context_object_name = 'Drawings'
+    ordering = ['-date_posted']
+
 
 def Profile(request):
     return render(request, 'Art/profile.html')
